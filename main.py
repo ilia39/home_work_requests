5110c1551d325c3f9841962a8fc2cc3910c8c032
Ilia <zaicne@inbox.ru>
2021-04-25 14:21:29 +0200

    Fixed endless cycle stackoverflow

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ superhero_list = ['Hulk', 'Captain America', 'Thanos']
 
 
 def yandex_disc_put(file, fname_disc):
-    upload_url_request = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+    upload_url_request = \
+        'https://cloud-api.yandex.net/v1/disk/resources/upload'
     headers = {
         'Content-type': 'application/json',
         'Authorization': f'OAuth {oauth_token}'
     }
     params = {'path': fname_disc, 'overwrite': 'true'}
-    answer_data = requests.get(upload_url_request, headers=headers, params=params)
+    answer_data = requests.get(upload_url_request,
+                               headers=headers, params=params)
     link = answer_data.json().get('href')
     response = requests.put(link, data=open(file, 'rb'))
     response.raise_for_status()
@@ -37,10 +39,12 @@ def get_smartest_sh(superhero_list):
 
 def get_stackoverflow_questions():
     date_str = datetime.datetime.utcnow().strftime('%Y-%m-%d')
-    date_timestamp_str = datetime.datetime.strptime(date_str, '%Y-%m-%d').strftime("%s")
+    date_timestamp_str = \
+        datetime.datetime.strptime(date_str, '%Y-%m-%d').strftime("%s")
     date_deep2 = datetime.datetime.utcnow().date() - datetime.timedelta(2)
     date_deep2_str = date_deep2.strftime('%Y-%m-%d')
-    date_deep2_timestamp = datetime.datetime.strptime(date_deep2_str, '%Y-%m-%d').strftime("%s")
+    date_deep2_timestamp = \
+        datetime.datetime.strptime(date_deep2_str, '%Y-%m-%d').strftime("%s")
     params_base = {
         'site': 'stackoverflow', 'fromdate': date_deep2_timestamp,
         'todate': date_timestamp_str, 'tagged': 'Python', 'order': 'desc',
@@ -57,7 +61,7 @@ def get_stackoverflow_questions():
         with open('question_base.txt', 'a') as f:
             f.write(str(resp.json()['items']))
         page += 1
-        if resp.json()['has_more'] == 'False':
+        if resp.json()['has_more'] == False:
             work = False
 
 smartest_sh = get_smartest_sh(superhero_list)
